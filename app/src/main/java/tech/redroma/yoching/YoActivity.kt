d34bc4d962c68d26e449ab96a87af79cfcb72702
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-19 16:39:53 -0700

    Rewrites code in Kotlin

---
+++ app/src/main/java/tech/redroma/yoching/YoActivity.kt
@@ -0,0 +1,14 @@
+package tech.redroma.yoching
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class YoActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yo)
+    }
+}
