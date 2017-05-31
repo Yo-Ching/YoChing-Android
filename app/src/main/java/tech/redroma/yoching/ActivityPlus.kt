2ac3db11b85f5ae224162d79759bd83d5a808882
Wellington Moreno <jwellington.moreno@gmail.com>
2017-05-31 00:39:24 -0700

    Adds ActivityPlus.kt

---
+++ app/src/main/java/tech/redroma/yoching/ActivityPlus.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017 RedRoma, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.redroma.yoching
+
+import android.app.Activity
+import android.graphics.Typeface
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+
+internal val Activity.actionBarTitleView: TextView? get()
+{
+    return findViewById(R.id.yo_action_bar_title) as? TextView
+}
+
+/**
+ *
+ * @author SirWellington
+ */
+internal fun AppCompatActivity.setActionBarFont(typeface: Typeface, size: Int)
+{
+    val titleView = actionBarTitleView ?: return
+    titleView.typeface = typeface
+    titleView.textSize = size.toFloat()
+}
