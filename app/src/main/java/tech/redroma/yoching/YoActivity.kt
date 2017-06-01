012625890a2b7145d3079c319f712fe51b22f032
Wellington Moreno <jwellington.moreno@gmail.com>
2017-06-01 00:48:21 -0700

    Refactors some code

--- app/src/main/java/tech/redroma/yoching/YoActivity.kt
+++ app/src/main/java/tech/redroma/yoching/YoActivity.kt
@@ -2,18 +2,26 @@ package tech.redroma.yoching
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.widget.TextView
 
 class YoActivity : AppCompatActivity()
 {
+    private lateinit var actionBar: Toolbar
     private lateinit var prompt: TextView
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
 
+        setupView()
+    }
+
+    private fun setupView()
+    {
         setContentView(R.layout.activity_yo)
 
+        actionBar = findViewById(R.id.yo_action_bar) as Toolbar
         prompt = findViewById(R.id.yo_prompt) as TextView
         prompt.typeface = applicationContext.exoBlack()
 
