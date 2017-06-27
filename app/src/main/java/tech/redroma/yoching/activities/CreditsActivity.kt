d1fa2abebf8f86af67e03c8a90d37e5efa26e8aa
Wellington Moreno <jwellington.moreno@gmail.com>
2017-06-27 14:52:25 -0700

    Customizes the up button of the Credits Activity

--- app/src/main/java/tech/redroma/yoching/activities/CreditsActivity.kt
+++ app/src/main/java/tech/redroma/yoching/activities/CreditsActivity.kt
@@ -1,20 +1,63 @@
 package tech.redroma.yoching.activities
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.AppCompatImageButton
+import android.support.v7.widget.Toolbar
+import android.view.ViewGroup
+import android.widget.TextView
 import tech.redroma.yoching.R
+import tech.redroma.yoching.extensions.*
+import tech.redroma.yoching.views.ViewContainer
 
 class CreditsActivity : AppCompatActivity()
 {
+
+    private val views = Views()
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credits)
-    }
 
+        views.inflate()
+    }
 
     private inner class Views
     {
+        lateinit var yoActionBar: Toolbar
+        lateinit var yoActionBarTitle: TextView
+
+
+        fun inflate()
+        {
+            yoActionBar = findView(R.id.yo_action_bar)
+            yoActionBarTitle = findView(R.id.yo_action_bar_title)
+
+            setupUpButton()
+        }
+
+        fun setupUpButton()
+        {
+            //Action bar code
+            setSupportActionBar(yoActionBar)
+            supportActionBar?.setHomeButtonEnabled(true)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            title = ""
+            val backButton = resources.getDrawable(R.drawable.arrow_back)
+            supportActionBar?.setHomeAsUpIndicator(backButton)
+
+            val button = yoActionBar.firstChildWhere<AppCompatImageButton> { it is AppCompatImageButton } ?: return
+            button.setOnClickListener {
+                LOG.info("Back Button clicked!")
+            }
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP)
+            {
+                button.setBackgroundDrawable(resources.getDrawable(R.drawable.ripple_circular))
+            }
+
+        }
     }
 }
