7c75c976aee5c24dd1cda43a75e1aebc73951206
Wellington Moreno <jwellington.moreno@gmail.com>
2017-06-27 12:06:10 -0700

    Create CreditsActivity and layout file

---
+++ app/src/main/java/tech/redroma/yoching/activities/CreditsActivity.kt
@@ -0,0 +1,20 @@
+package tech.redroma.yoching.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import tech.redroma.yoching.R
+
+class CreditsActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credits)
+    }
+
+
+    private inner class Views
+    {
+    }
+}
