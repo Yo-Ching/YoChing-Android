bc4894245c5932e67247247773919b0075506fb5
Wellington Moreno <jwellington.moreno@gmail.com>
2017-06-27 15:52:11 -0700

    Sets up the typeface for the credits page

--- app/src/main/java/tech/redroma/yoching/activities/CreditsActivity.kt
+++ app/src/main/java/tech/redroma/yoching/activities/CreditsActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.AppCompatImageButton
 import android.support.v7.widget.Toolbar
+import android.view.ViewGroup
+import android.view.textservice.TextInfo
 import android.widget.TextView
-import tech.redroma.yoching.R
+import org.w3c.dom.Text
+import tech.redroma.yoching.*
 import tech.redroma.yoching.extensions.*
 
 class CreditsActivity : AppCompatActivity()
@@ -33,13 +36,61 @@ class CreditsActivity : AppCompatActivity()
         lateinit var yoActionBar: Toolbar
         lateinit var yoActionBarTitle: TextView
 
+        lateinit var truePlayer: TextView
+        lateinit var truePlayerDescription: TextView
+        lateinit var truePlayerContainer: ViewGroup
+
+        lateinit var hugh: TextView
+        lateinit var hughDescription: TextView
+        lateinit var hughContainer: ViewGroup
+
+        lateinit var wellington: TextView
+        lateinit var wellingtonDescription: TextView
+        lateinit var wellingtonContainer: ViewGroup
+
+        lateinit var marc: TextView
+        lateinit var marcDescription: TextView
+        lateinit var marcContainer: ViewGroup
+
+        lateinit var brendan: TextView
+        lateinit var brendanDescription: TextView
+        lateinit var brendanContainer: ViewGroup
+
+        lateinit var maya: TextView
+        lateinit var mayaDescription: TextView
+        lateinit var mayaContainer: ViewGroup
 
         fun inflate()
         {
             yoActionBar = findView(R.id.yo_action_bar)
             yoActionBarTitle = findView(R.id.yo_action_bar_title)
 
+            truePlayer = findView(R.id.true_player)
+            truePlayerDescription = findView(R.id.true_player_description)
+            truePlayerContainer = findView(R.id.true_player_container)
+
+            hugh = findView(R.id.hugh)
+            hughDescription = findView(R.id.hugh_description)
+            hughContainer = findView(R.id.hugh_container)
+
+            wellington = findView(R.id.wellington)
+            wellingtonDescription = findView(R.id.wellington_description)
+            wellingtonContainer = findView(R.id.wellington_container)
+
+            marc = findView(R.id.marc)
+            marcDescription = findView(R.id.marc_description)
+            marcContainer = findView(R.id.marc_container)
+
+            brendan = findView(R.id.brendan)
+            brendanDescription = findView(R.id.brendan_description)
+            brendanContainer = findView(R.id.brendan_container)
+
+            maya = findView(R.id.maya)
+            mayaDescription = findView(R.id.maya_description)
+            mayaContainer = findView(R.id.marc_container)
+
             setupUpButton()
+            setFonts()
         }
 
         fun setupUpButton()
@@ -64,5 +115,18 @@ class CreditsActivity : AppCompatActivity()
             }
 
         }
+
+        fun setFonts()
+        {
+            val titleFont = exoDemiBold()
+            val subtitleFont = exoRegular()
+
+            listOf(truePlayer, hugh, wellington, marc, brendan, maya)
+                    .forEach { it.typeface = titleFont }
+
+            listOf(truePlayerDescription, hughDescription, wellingtonDescription, marcDescription, brendanDescription, mayaDescription)
+                    .forEach { it.typeface = subtitleFont }
+        }
+
     }
 }
